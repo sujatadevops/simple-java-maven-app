00022c7dde5f76694101586820b880b0fc0a270b
sujatadevops <99498548+sujatadevops@users.noreply.github.com>
2022-03-04 18:16:41 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Helloooooooooooooooooooooooooooooooooo World!";
+    private final String message = "Helloooooooooooooooooooooooooooooo World!";
 
     public App() {}
 
