c1e4f0f959e08eef841e099a7ccf2b1eac0876f8
sujatadevops <99498548+sujatadevops@users.noreply.github.com>
2022-03-04 18:11:04 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hellooooooooooooooooooooooooooooooooo World!";
+    private final String message = "Helloooooooooooooooooooooooooooooooooo World!";
 
     public App() {}
 
